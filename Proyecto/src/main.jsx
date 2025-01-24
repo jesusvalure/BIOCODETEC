7b683af1f82b06bb4d315e640bdef99bee826775
chuz0 <115124922+chuz0@users.noreply.github.com>
2025-01-24 11:11:48 -0600

    Archivos que tuvieron actualizacion

--- Proyecto/src/main.jsx
+++ Proyecto/src/main.jsx
@@ -1,6 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 
-const root = createRoot(document.getElementById('root'))
-root.render("Hello, World!")
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
